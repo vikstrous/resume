60c515c6492e9f094a27cee2a581d0ff58555a9a
Viktor Stanchev <me@viktorstanchev.com>
2012-05-14 10:43:36 -0400

    use port 3000 instead of 80

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function(req, res) {
   });
 });
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Listening on ' + port);
 });
